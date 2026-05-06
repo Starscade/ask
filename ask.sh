706f21e56f862453eec8a87fc2e5153c300832c3
Starscade <angus@starsca.de>
2026-05-06 09:09:05 -0700

    Handle API error responses.

--- ask.sh
+++ ask.sh
@@ -76,7 +76,4 @@ curl -LsS "$GEMINI_URL" \
 	-H "x-goog-api-key: ${GEMINI_API_KEY}" \
 	-H 'Content-Type: application/json' \
 	-d "$GEMINI_JSON" \
-	| {
-		jq -er '.candidates[0].content.parts[0].text' \
-		|| give_up 'No more credits!'
-	}
+	| jq -er '.candidates[0].content.parts[0].text // .error.message'
