f5d747180853b3856ecb1ac29eaede691101de87
Starscade <angus@starsca.de>
2026-07-25 14:22:39 -0700

    Allow stdin and/or args.

--- ask.sh
+++ ask.sh
@@ -17,22 +17,26 @@ to_json() {
 check_command curl
 check_command jq
 
-test -n "$1" || give_up "You didn't ask anything."
+RAW_USER_PROMPT="$*"
+test ! -t 0 && RAW_USER_PROMPT="${RAW_USER_PROMPT}\n\n$(cat)"
 
-if test -s .env; then
+test -z "$RAW_USER_PROMPT" \
+	&& give_up "You didn't ask anything."
+
+test -s .env && {
 	set -a
 	. .env
 	set +a
-fi
+}
 
-test ! -z "$GEMINI_API_KEY" \
-	|| give_up "\033[1mGEMINI_API_KEY\033[0m not set."
+test -z "$GEMINI_API_KEY" \
+	&& give_up "\033[1mGEMINI_API_KEY\033[0m not set."
 
 GEMINI_MODEL=${GEMINI_MODEL:-'gemini-flash-lite-latest'}
 GEMINI_PERSONA=${GEMINI_PERSONA:-'You respond exclusively in plaintext code snippets that can be executed (or compiled) as is. Never format your responses using markdown. If no language is specified, write code in POSIX-complient sh (or PostgreSQL if dealing with SQL). Always use the most portable syntax. Otherwise, write the code in the language that the user mentions.'}
 GEMINI_PERSONA_JSON=$(to_json "$GEMINI_PERSONA")
 GEMINI_URL='https://generativelanguage.googleapis.com/v1beta/interactions?alt=sse'
-USER_PROMPT_JSON="$(to_json "$@")"
+USER_PROMPT_JSON="$(to_json "$RAW_USER_PROMPT")"
 
 RAW_OBJ='{
 	"generation_config": {
